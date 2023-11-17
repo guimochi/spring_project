9769be36b366d6066eab278d8cabb52913ca0a89
guimochi <yamamoto.guilherme@outlook.com>
2023-11-17 23:36:38 +0100

    fix missing files in matching

--- matching/src/main/java/vinci/stock/matching/MatchingService.java
+++ matching/src/main/java/vinci/stock/matching/MatchingService.java
@@ -1,11 +1,27 @@
 package vinci.stock.matching;
 
 import org.springframework.stereotype.Service;
+import vinci.stock.matching.models.Order;
+import vinci.stock.matching.models.Side;
+import vinci.stock.matching.repositories.ExecutionProxy;
+import vinci.stock.matching.repositories.OrderProxy;
 
 @Service
 public class MatchingService {
 
-  public void match(String ticker) {
+  private final OrderProxy orderProxy;
+  private final ExecutionProxy executionProxy;
 
+  public MatchingService(OrderProxy orderProxy, ExecutionProxy executionProxy) {
+    this.orderProxy = orderProxy;
+    this.executionProxy = executionProxy;
+  }
+
+  public void match(String ticker) {
+    Iterable<Order> ordersSideBuy = orderProxy.readAllOpenByTickerAndSide(ticker,
+        Side.BUY.toString()).getBody();
+    Iterable<Order> ordersSideSell = orderProxy.readAllOpenByTickerAndSide(ticker,
+        Side.SELL.toString()).getBody();
+//    TODO: implement matching algorithm
   }
 }
---
+++ matching/src/main/java/vinci/stock/matching/repositories/ExecutionProxy.java
@@ -0,0 +1,16 @@
+package vinci.stock.matching.repositories;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Repository
+@FeignClient(name = "execution")
+public interface ExecutionProxy {
+
+  @PostMapping("/execute/{ticker}/{seller}/{buyer}")
+  ResponseEntity<Void> execute(@PathVariable String ticker, @PathVariable String seller,
+      @PathVariable String buyer);
+}
