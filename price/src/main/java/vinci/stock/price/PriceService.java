66cb445ffdf546b005d1919f591b9ca6124112bf
Alicia Boltryk <boltryk.alicia@gmail.com>
2023-11-30 14:34:59 +0100

    add javadoc

--- price/src/main/java/vinci/stock/price/PriceService.java
+++ price/src/main/java/vinci/stock/price/PriceService.java
@@ -13,12 +13,21 @@ public class PriceService {
     this.repository = repository;
   }
 
+  /**
+   * Get the price of a position
+   * @param ticker Ticker of the position
+   * @return Price of the position
+   */
   public int getOne(String ticker) {
     Optional<Price> p = repository.findById(ticker);
     return p.map(Price::getPrice).orElse(1);
   }
 
-  // update existing price or create one
+  /**
+   * Update the price of a position
+   * If the position does not exist, it is created
+   * @param p Position with updated price
+   */
   public void updateOne(Price p) {
     repository.save(p);
   }
